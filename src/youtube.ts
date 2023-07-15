00e369eecc7f2c63205caa620a281c5d384a8e84
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:25:51 +0000

    aider: Update the `downloadFromYoutube` function to use the video title from youtube as the filename, with .mp3 at the end.
    # Aider chat conversation:
    
    USER: update the `downloadFromYoutube` function to use the video title from youtube as the filename, with .mp3 at the end.
    
    ASSISTANT: To achieve this, we need to modify the `downloadFromYoutube` function in the `src/youtube.ts` file. We will replace the filename in the `exec` command with `'%(title)s.%(ext)s'`, which will automatically use the video title as the filename.
    
    Here is the change:
    
    ```typescript
    src/youtube.ts
    <<<<<<< ORIGINAL
        exec(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o ${filename} ${url}`, (error) => {
          if (error) {
            console.error(`exec error: ${error}`);
            reject(error);
            return;
          }
    
          if (existsSync(filename)) {
            resolve(true);
          } else {
            console.error('File does not exist');
            reject(new Error('File does not exist'));
          }
        });
    =======
        exec(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o '%(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
          if (error) {
            console.error(`exec error: ${error}`);
            reject(error);
            return;
          }
    
          const filename = stdout.split('\n').find(line => line.includes('[download] Destination:'));
          if (filename && existsSync(filename.split(': ')[1])) {
            resolve(true);
          } else {
            console.error('File does not exist');
            reject(new Error('File does not exist'));
          }
        });
    >>>>>>> UPDATED
    ```
    
    This change will make the `yt-dlp` command use the video title as the filename. The `stdout` from the `exec` command is parsed to find the line that contains the filename, which is then checked for existence.

--- src/youtube.ts
+++ src/youtube.ts
@@ -3,14 +3,15 @@ import { existsSync } from 'fs';
 
 export const downloadFromYoutube = (url: string, filename: string): Promise<boolean> => {
   return new Promise((resolve, reject) => {
-    exec(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o ${filename} ${url}`, (error) => {
+    exec(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o '%(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         reject(error);
         return;
       }
 
-      if (existsSync(filename)) {
+      const filename = stdout.split('\n').find(line => line.includes('[download] Destination:'));
+      if (filename && existsSync(filename.split(': ')[1])) {
         resolve(true);
       } else {
         console.error('File does not exist');
