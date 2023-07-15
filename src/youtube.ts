a8f3c3bcd58a62d8b3a6649907c441c7c7f66d44
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:53:55 +0000

    Refactor downloadFromYoutube function to use double quotes for imports and split long lines for better readability.
    # Aider chat conversation:

--- src/youtube.ts
+++ src/youtube.ts
@@ -1,39 +1,48 @@
-import { exec } from 'child_process';
-import { existsSync, unlinkSync } from 'fs';
-import { uploadToS3 } from './upload';
+import { exec } from "child_process";
+import { existsSync, unlinkSync } from "fs";
+import { uploadToS3 } from "./upload";
 
-export const downloadFromYoutube = (url: string): Promise<{ success: boolean; filename: string }> => {
+export const downloadFromYoutube = (
+  url: string
+): Promise<{ success: boolean; filename: string }> => {
   return new Promise((resolve, reject) => {
     console.log(`Starting download from YouTube for URL: ${url}`);
-    exec(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o '%(title)s.%(ext)s' ${url}`, (error, stdout, stderr) => {
-      console.log('Download completed');
-      if (error) {
-        console.error(`exec error: ${error}`);
-        reject({ success: false, filename: '' });
-        return;
-      }
+    exec(
+      `yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o '%(title)s.%(ext)s' ${url}`,
+      (error, stdout, stderr) => {
+        console.log("Download completed");
+        if (error) {
+          console.error(`exec error: ${error}`);
+          reject({ success: false, filename: "" });
+          return;
+        }
 
-      let outputLine = stdout.split('\n').find(line => line.includes('[download] Destination:'));
-      if (outputLine) {
-        let outputFilename = outputLine.split(': ')[1].replace('.webm', '.mp3');
-        if (existsSync(outputFilename)) {
-          console.log(`Starting upload to S3 for file: ${outputFilename}`);
-          uploadToS3(outputFilename)
-            .then(() => {
-              console.log('Upload completed');
-              console.log(`Deleting local file: ${outputFilename}`);
-              unlinkSync(outputFilename);
-              resolve({ success: true, filename: outputFilename });
-            })
-            .catch((uploadErr) => {
-              console.error(`Error uploading file: ${uploadErr}`);
-              reject({ success: false, filename: '' });
-            });
-        } else {
-          console.error('File does not exist');
-          reject({ success: false, filename: '' });
+        let outputLine = stdout
+          .split("\n")
+          .find((line) => line.includes("[download] Destination:"));
+        if (outputLine) {
+          let outputFilename = outputLine
+            .split(": ")[1]
+            .replace(".webm", ".mp3");
+          if (existsSync(outputFilename)) {
+            console.log(`Starting upload to S3 for file: ${outputFilename}`);
+            uploadToS3(outputFilename)
+              .then(() => {
+                console.log("Upload completed");
+                console.log(`Deleting local file: ${outputFilename}`);
+                unlinkSync(outputFilename);
+                resolve({ success: true, filename: outputFilename });
+              })
+              .catch((uploadErr) => {
+                console.error(`Error uploading file: ${uploadErr}`);
+                reject({ success: false, filename: "" });
+              });
+          } else {
+            console.error("File does not exist");
+            reject({ success: false, filename: "" });
+          }
         }
       }
-    });
+    );
   });
 };
