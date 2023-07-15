7d846222025c468b3148b33ecba0dc28ab68ae34
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:46:02 +0000

    Refactor uploadToS3 function to include additional parameters for endpoint and region.
    # Aider chat conversation:

--- src/upload.ts
+++ src/upload.ts
@@ -1,19 +1,22 @@
 import AWS from "aws-sdk";
 import fs from "fs";
+import "./envVars";
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.ACCESS_KEY_SECRET,
+  endpoint: "https://s3.us-west-004.backblazeb2.com",
+  region: "us-west-004",
 });
 
 export const uploadToS3 = async (filename: string): Promise<void> => {
   const fileContent = fs.readFileSync(filename);
 
   const params = {
-  Bucket: process.env.BUCKET_NAME,
-  Key: `dj-set-downloader/${filename}`,
-  Body: fileContent,
-};
+    Bucket: process.env.BUCKET_NAME,
+    Key: `dj-set-downloader/${filename}`,
+    Body: fileContent,
+  };
 
   try {
     const data = await s3.upload(params).promise();
