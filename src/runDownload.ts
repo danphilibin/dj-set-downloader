7dc5c99a0607e88caf5d1ff651fc16841de88a0d
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:33:25 +0000

    aider: Updated `runDownload.ts` to remove the file after it downloads and to throw an error if the file is not found.
    # Aider chat conversation:
    
    USER: update src/runDownload.ts to remove the file after it downloads, and to throw an error if the file is not found
    
    ASSISTANT: To update the `runDownload.ts` file to remove the file after it downloads and to throw an error if the file is not found, we need to modify the `src/runDownload.ts` file. We will use the `unlinkSync` function from the `fs` module to remove the file.
    
    Here is the change:
    
    ```typescript
    src/runDownload.ts
    <<<<<<< ORIGINAL
    import { downloadFromYoutube } from './youtube';
    
    const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
    downloadFromYoutube(testUrl)
      .then(({ success, filename }) => {
        if (success) {
          console.log(`Download successful: ${filename}`);
        } else {
          console.log('Download failed');
        }
      })
      .catch(({ success, filename }) => {
        console.error(`Download failed: ${filename}`);
      });
    =======
    import { downloadFromYoutube } from './youtube';
    import { unlinkSync, existsSync } from 'fs';
    
    const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
    downloadFromYoutube(testUrl)
      .then(({ success, filename }) => {
        if (success) {
          console.log(`Download successful: ${filename}`);
          unlinkSync(filename);
          console.log(`File ${filename} removed`);
        } else {
          console.log('Download failed');
        }
      })
      .catch(({ success, filename }) => {
        if (existsSync(filename)) {
          console.error(`Download failed: ${filename}`);
        } else {
          throw new Error(`File ${filename} not found`);
        }
      });
    >>>>>>> UPDATED
    ```
    
    This change modifies the `then` and `catch` callbacks of the `downloadFromYoutube` function call to handle the new requirements. The `then` callback now removes the file after a successful download and logs a message indicating that the file has been removed. The `catch` callback now throws an error if the file is not found.

--- src/runDownload.ts
+++ src/runDownload.ts
@@ -1,14 +1,21 @@
 import { downloadFromYoutube } from './youtube';
+import { unlinkSync, existsSync } from 'fs';
 
 const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
 downloadFromYoutube(testUrl)
   .then(({ success, filename }) => {
     if (success) {
       console.log(`Download successful: ${filename}`);
+      unlinkSync(filename);
+      console.log(`File ${filename} removed`);
     } else {
       console.log('Download failed');
     }
   })
   .catch(({ success, filename }) => {
-    console.error(`Download failed: ${filename}`);
+    if (existsSync(filename)) {
+      console.error(`Download failed: ${filename}`);
+    } else {
+      throw new Error(`File ${filename} not found`);
+    }
   });
