bcab113252db7cb1df1548a7b5b98b066f82b50f
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:32:47 +0000

    aider: Update the runDownload file with the new response format for the downloadFromYoutube function.
    # Aider chat conversation:
    
    USER: update the runDownload file with the new response format for the downloadFromYoutube function
    
    ASSISTANT: To update the `runDownload.ts` file to handle the new response format from the `downloadFromYoutube` function, we need to modify the `src/runDownload.ts` file.
    
    Here is the change:
    
    ```typescript
    src/runDownload.ts
    <<<<<<< ORIGINAL
    import { downloadFromYoutube } from './youtube';
    
    const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
    downloadFromYoutube(testUrl)
      .then((exists) => console.log(`Download successful: ${exists}`))
      .catch((error) => console.error(`Download failed: ${error}`));
    =======
    import { downloadFromYoutube } from './youtube';
    
    const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
    downloadFromYoutube(testUrl)
      .then(({ success, filename }) => {
        if (success) {
          console.log(`Download successful: ${filename}`);
        } else {
          console.log('Download failed');
        }
      })
      .catch(({ success, filename }) => {
        console.error(`Download failed: ${filename}`);
      });
    >>>>>>> UPDATED
    ```
    
    This change modifies the `then` and `catch` callbacks of the `downloadFromYoutube` function call to handle the new response format. The `then` callback now destructures the `success` and `filename` properties from the resolved value and logs a success message with the filename if `success` is `true`, and a failure message otherwise. The `catch` callback now destructures the `success` and `filename` properties from the rejected value and logs a failure message with the filename.

--- src/runDownload.ts
+++ src/runDownload.ts
@@ -2,5 +2,13 @@ import { downloadFromYoutube } from './youtube';
 
 const testUrl = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
 downloadFromYoutube(testUrl)
-  .then((exists) => console.log(`Download successful: ${exists}`))
-  .catch((error) => console.error(`Download failed: ${error}`));
+  .then(({ success, filename }) => {
+    if (success) {
+      console.log(`Download successful: ${filename}`);
+    } else {
+      console.log('Download failed');
+    }
+  })
+  .catch(({ success, filename }) => {
+    console.error(`Download failed: ${filename}`);
+  });
