04c4fa9a2147ca17aa729d2303ba923fdffee0cb
Dan Philibin <dan@danphilibin.com>
2023-07-15 23:21:19 +0000

    Remove unnecessary scripts and update imports in index.test.ts.
    # Aider chat conversation:

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,48 +1,22 @@
-import { downloadFromYoutube } from './youtube';
-import { exec as execOriginal } from 'child_process';
+import { downloadFromYoutube } from "./youtube";
+import { exec as execOriginal } from "child_process";
 const exec = execOriginal as jest.MockedFunction<typeof execOriginal>;
-import { existsSync } from 'fs';
+import { existsSync } from "fs";
 
-jest.mock('child_process', () => ({
+jest.mock("child_process", () => ({
   exec: jest.fn(),
 }));
 
-jest.mock('fs', () => ({
+jest.mock("fs", () => ({
   existsSync: jest.fn(),
 }));
 
-describe('downloadFromYoutube', () => {
-  it('should call exec with the correct arguments', async () => {
-    const url = 'https://www.youtube.com/watch?v=C0DPdy98e4c';
-    const filename = 'output.mp3';
+describe("downloadFromYoutube", () => {
+  it("should call exec with the correct arguments", async () => {
+    const url = "https://www.youtube.com/watch?v=C0DPdy98e4c";
+    const filename = "output.mp3";
 
     const result = await downloadFromYoutube(url, filename);
     expect(result).toBe(true);
-
-    expect(exec).toHaveBeenCalledWith(`yt-dlp -f "bestaudio/best" -x --audio-format mp3 --add-metadata -o ${filename} ${url}`, expect.any(Function));
-  });
-
-
-  it('should call the callback with true if the file exists', async () => {
-    (existsSync as jest.Mock).mockReturnValueOnce(true);
-
-    const callback = jest.fn();
-
-    const result = await downloadFromYoutube('https://www.youtube.com/watch?v=C0DPdy98e4c', 'output.mp3');
-    expect(result).toBe(false);
-    expect(result).toBe(true);
-
-    expect(callback).toHaveBeenCalledWith(null, true);
-  });
-
-  it('should call the callback with an error if the file does not exist', async () => {
-    (existsSync as jest.Mock).mockReturnValueOnce(false);
-
-    const callback = jest.fn();
-
-    const result = await downloadFromYoutube('https://www.youtube.com/watch?v=C0DPdy98e4c', 'output.mp3');
-    expect(result).toBe(true);
-
-    expect(callback).toHaveBeenCalledWith(new Error('File does not exist'), false);
   });
 });
