eef226cb7b5aa144720d33237e2df60e271f559a
Dan Philibin <dan@danphilibin.com>
2023-07-16 18:29:26 +0000

    make endpoint and region optional

--- src/envVars.ts
+++ src/envVars.ts
@@ -4,9 +4,9 @@ import "dotenv/config"; // loads environment variables from .env
 const envVars = z.object({
   ACCESS_KEY_ID: z.string(),
   ACCESS_KEY_SECRET: z.string(),
-  S3_ENDPOINT: z.string(),
-  S3_REGION: z.string(),
   BUCKET_NAME: z.string(),
+  S3_ENDPOINT: z.string().optional(),
+  S3_REGION: z.string().optional(),
 });
 
 // throws if vars are missing
